97558bf00800105a891c94477ee0ddfb7ea79cce
Fadi Makhoul <fadi_makhoul1@hotmail.com>
2021-06-17 12:31:11 +0300

    Added instructions to the README file and solved suggested issue

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# fadi-noor
+# fadi-noor - HangMan-Game
+
+Hangman is an old paper and pencil guessing game for two or more players. One player thinks of a word, phrase or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses.
+
+Though the origins of the game are unknown, a variant is mentioned in a book of children's games assembled by Alice Gomme in 1894 called Birds, Beasts, and Fishes. In France the game is drawn with a man on a guillotine and the rest is drawn, minus a head.
+
+We decided to select this a as our project for the first week, and built a webpagage that simulates the game,actually can be played and either you win or lose!
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/* declearing variables  */ 
 var wordsBank = ["academic" ,"athletic", "camping", "conflict", "electric", "innocent", "language", "positive", "tactical", "workshop"]
-var word = wordsBank[Math.floor(Math.random() * wordsBank.length)];
+
+var word = wordsBank[Math.floor(Math.random() * wordsBank.length)]; // randomly select a word from the wordBank array of words
 let mistakes =0; 
 let corrects = 0;
 var images = ['https://www.gamehangman.com/sites/all/modules/hangman/images/Hangman-1.png',
@@ -10,19 +12,21 @@ var images = ['https://www.gamehangman.com/sites/all/modules/hangman/images/Hang
 'https://www.gamehangman.com/sites/all/modules/hangman/images/Hangman-6.png',
 'https://www.gamehangman.com/sites/all/modules/hangman/images/Hangman-hung.png',
 'https://www.pngkit.com/png/full/967-9670677_game-over-monochrome.png'];
+
 let mainimage = document.getElementById('mainPicture');
 let wordPuzzle = document.getElementById('wordPuzzle');
 let lettersP = document.getElementById("letters");
 let wordp = document.getElementById("word");
 
-for (let i = 0; i < lettersP.childNodes.length; i++) {
+/* Loop that Checks if a letter is selected*/
+for (let i = 0; i < lettersP.childNodes.length; i++) { 
     const child = lettersP.childNodes[i];
    
     child.addEventListener('click',function check(){
         let cor = false;
-        if(child.className === 'let' && mistakes < 7)
+        if(child.className === 'let' && mistakes < 7)// if the letter still unselected and the player hasn't lost yet 
         {
-            for (let j = 0; j < word.length; j++) {
+            for (let j = 0; j < word.length; j++) { // checking every letter in the selected word, and checks if any of it is equivalent to the selected letter.
                 if(word[j]===child.textContent.toLowerCase()){
                     wordp.children[j].className = 'Known';
                     wordp.children[j].textContent = word[j].toLocaleUpperCase();
@@ -33,13 +37,12 @@ for (let i = 0; i < lettersP.childNodes.length; i++) {
             }
         
 
-            if(!cor)
-            {
+            if(!cor && corrects < 7){ // if the selected letter is wrong but the player havent lost yet
                 child.className = 'wrong';
                 mistakes++; 
-                mainimage.src = images[mistakes];  
+                mainimage.src = images[mistakes];
             }
-            if(corrects===8)
+            if(corrects===8) // win 
             {
                 mainimage.src = 'https://www.pngitem.com/pimgs/m/41-416521_left-troll-face-rage-meme-troll-face-hd.png';
             }
