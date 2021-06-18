5d762e461d50e6311b2484e9cb86cd97154748d5
noor-sawaed <n.swa3ed.swa3ed@gmail.com>
2021-06-18 21:44:24 +0300

    Noor solo update 0.2 -- remove new game button + added hint feature.

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 var wordsBank = ["academic", "dog","athletic", "camping", "conflict", "electric", "innocent", "language",
 "positive", "tactical", "workshop","sting","bee","milk", "various", "feather", "purple", "thin", "island", "knife","travel",
 "base","worm","haircut","rain","daughter","change","finger","exchange","fight"];
-
-var word = wordsBank[Math.floor(Math.random() * wordsBank.length)]; // randomly select a word from the wordBank array of words
+var HintBank = ["Relating to education and scholarship.", "Best friend for life","Someone who likes to do sport", "Living In the wild",
+"a serious disagreement or argument, typically a protracted one", "SomeThing that ZAP's", "A baby feature", "How Do we comunicate?","+",
+"relating to or constituting actions carefully planned to gain a specific military end", "Mario: So now please Start working on this ____.",
+"Its just a bee what can can it do to you? MAN UP","A striped bug","Moooo", "more than one", "Its not like you have a ___ on your head !!","it may be a color,or not! ",
+"Narrow","Trapped by water from all angles", "Don't cut your self with that","Every one's hobby Pretty much","the lowest part or edge of something",
+"Man i love fishing! did you bring the BAIT???","20₪ for men, 1500₪ for wemen","The best thing in winter","A reason to get married",
+"make (someone or something) different","You have 20 of them.","An act of giving one thing and receiving another (especially of the same type or value) in return",
+"Take part in a violent struggle involving the exchange of physical blows or the use of weapons."];
+var randomWordIndex = Math.floor(Math.random() * wordsBank.length);
+var word = wordsBank[randomWordIndex]; // randomly select a word from the wordBank array of words
 let mistakes = 0;
 let corrects = 0;
 let allLetters = "abcdefghijklmnopqrstuvwxyz";
@@ -20,6 +28,7 @@ let mainimage = document.getElementById('mainPicture');
 let wordPuzzle = document.getElementById('wordPuzzle');
 let lettersP = document.getElementById("letters");
 let wordp = document.getElementById("word");
+let HintT = document.getElementById("hintText");
 document.getElementById("number").textContent = word.length+"";
 document.getElementById("opp").textContent = 7-mistakes+"";
 
@@ -37,10 +46,25 @@ for (let a = 0; a < allLetters.length; a++) {
     lettersP.appendChild(newDiv);
     newDiv.classList = 'let';
 }
+// adding  hint button 
+var HintDiv = document.createElement('div');
+HintDiv.textContent = '?';
+lettersP.appendChild(HintDiv);
+HintDiv.classList = 'hintButton';
+HintDiv.addEventListener('mouseenter',function getHint(){
+    
+        // add a hint text from the hint array for each word
+        HintT.textContent = HintBank[randomWordIndex];
+
+        // remove the hint after 3 seconds
+        setTimeout(function() {
+            HintT.textContent = "";
+    }, 3000);
+}, false);
 
 
 /* Loop that Checks if a letter is selected*/
-for (let i = 0; i < lettersP.childNodes.length; i++) {
+for (let i = 0; i < lettersP.childNodes.length-1; i++) {
     const child = lettersP.childNodes[i];
     child.addEventListener('click', function check() {
         let cor = false;
